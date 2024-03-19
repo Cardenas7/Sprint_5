643150d16e02ea0e794586d75e14a5b47a5d4276
Cardenas17 <jose.cardenasmu@anahuac.mx>
2024-03-19 01:26:02 -0600

    commit

--- app.py
+++ app.py
@@ -18,21 +18,32 @@ st.write(car_data)
 # filtro_marca = car_data[car_data['model'] == marca]
 
 # Iniciando otra sección
-st.subheader('Ahora vamos a examinar la relación del precio con otras variables')
+st.subheader('Ahora vamos a conocer más sobre este conjuntos de datos')
 
-# Visualización de datos adicionales
-st.subheader('Gráfico de Barras por Marca')
-fig_bar = px.bar(car_data, x='model', y='price')
-st.plotly_chart(fig_bar)
+# Estadísticas descriptivas del precio
+st.subheader("Estadísticas descriptivas del precio")
+st.write(f"Precio promedio: {round(car_data['price'].mean())}")
+st.write(f"Mediana del precio: {round(car_data['price'].median())}")
+st.write(f"Desviación Estándar del precio: {round(car_data['price'].std())}")
+
+# Gráfico de caja del precio    
+st.subheader("Analizando la distribución del precio")
+price_boxplot=px.box(car_data, x='price', labels={"price":"Precio"})
+st.plotly_chart(price_boxplot, use_container_width=True)
+
+# # Visualización de datos adicionales
+# st.subheader('Gráfico de Barras por Marca')
+# fig_bar = px.bar(car_data, x='model', y='price')
+# st.plotly_chart(fig_bar)
 
 # Primer botón
-build_histogram = st.checkbox('Construir un histograma')
+build_histogram = st.checkbox('Histograma del kilometraje')
 if build_histogram: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Esta es la frecuencia del kilometraje observada en los vehículos')
             
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer", labels={"odometer":"Kilometraje registrado"})
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -42,7 +53,7 @@ if build_histogram: # al hacer clic en el botón
 build_scatter=st.checkbox("Relación precio-odometro")
 if build_scatter: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('¿Notas alguna relación entre el kilometraje y el precio de los vehículos?')
             
     # crear un histograma
     fig_2 = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
@@ -51,17 +62,41 @@ if build_scatter: # al hacer clic en el botón
     st.plotly_chart(fig_2, use_container_width=True)
     # use_container_width=True:  útil para garantizar que el gráfico se ajuste correctamente al diseño de la página y se vea bien en diferentes tamaños de pantalla.
 
-
-# Segundo botón
-build_scatter_2=st.checkbox("Relación Precio-año del modelo")
-if build_scatter_2: # al hacer clic en el botón
+# Tercer botón
+build_histogram_2=st.checkbox("Distribución del estado de los vehículos ")
+if build_scatter: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    st.write('¿Notas alguna relación entre el kilometraje y el precio de los vehículos?')
             
     # crear un histograma
-    fig_3 = px.scatter(car_data, x="model_year", y="price") # crear un gráfico de dispersión
+    fig_3 = px.histogram(car_data, x="condition",labels={"price":"Precio"})
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig_3, use_container_width=True)
+
+# Cuarto botón
+build_scatter_2=st.checkbox("Relación Precio-año del modelo")
+if build_scatter_2: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('¿Observas alguna relación entre el precio y el año del modelo?')
+            
+    # crear un histograma
+    fig_4 = px.scatter(car_data, x="model_year", y="price") # crear un gráfico de dispersión
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_4, use_container_width=True)
     # use_container_width=True:  útil para garantizar que el gráfico se ajuste correctamente al diseño de la página y se vea bien en diferentes tamaños de pantalla.
+
+# Quinto botón
+build_circle=st.checkbox("Distribución de tipos de combustible")
+if build_circle: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('¿Cuál es el tipo de combustible aceptado con mayor presencia en el conjunto de datos?')
+            
+    # crear un histograma
+    fig_pie = px.pie(car_data, names='fuel')
+        
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_pie, use_container_width=True)
+
 
